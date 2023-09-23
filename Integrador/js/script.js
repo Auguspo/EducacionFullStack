939120cd3ab8d72706b5b89b32968c2f2714a57a
Auguspo <augustpolet@gmail.com>
2023-09-23 19:28:08 -0300

    integrador parte 2

--- Integrador/js/script.js
+++ Integrador/js/script.js
@@ -1,17 +1,52 @@
-// Seleccionar todas las cards de productos
-const productCards = document.querySelectorAll('.product-card');
-
-// Agregar un evento de mouseover para cada card
-productCards.forEach((card) => {
-    card.addEventListener('mouseover', () => {
-        // Aplicar una transformación de escala al pasar el mouse sobre la card
-        card.style.transform = 'scale(1.05)';
-        card.style.transition = 'transform 0.3s ease';
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submitButton");
+    const productForm = document.getElementById("product-form");
+  
+    submitButton.addEventListener("click", function () {
+      // Obtén los valores del formulario
+      const nombre = document.getElementById("nombre").value;
+      const precio = document.getElementById("precio").value;
+      const stock = document.getElementById("stock").value;
+      const marca = document.getElementById("marca").value;
+      const categoria = document.getElementById("categoria").value;
+      const descripcion_corta = document.getElementById("descripcion_corta").value;
+      const descripcion_larga = document.getElementById("descripcion_larga").value;
+      const edad_desde = document.getElementById("edad_desde").value;
+      const edad_hasta = document.getElementById("edad_hasta").value;
+  
+      // Crea un objeto con los datos del producto
+      const producto = {
+        nombre,
+        precio,
+        stock,
+        marca,
+        categoria,
+        descripcion_corta,
+        descripcion_larga,
+        edad_desde,
+        edad_hasta,
+      };
+  
+      // Almacena el objeto en el almacenamiento local del navegador
+      localStorage.setItem("nuevoProducto", JSON.stringify(producto));
+  
+      // Redirige a index.html
+      window.location.href = "index.html";
     });
-
-    card.addEventListener('mouseout', () => {
-        // Restaurar la escala original cuando se retira el mouse
-        card.style.transform = 'scale(1)';
-        card.style.transition = 'transform 0.3s ease';
-    });
-});
+  
+    // Extrae el producto del almacenamiento local (si existe)
+    const productoJSON = localStorage.getItem("nuevoProducto");
+  
+    if (productoJSON) {
+      const producto = JSON.parse(productoJSON);
+  
+      // Modifica la última tarjeta en index.html con los datos del producto
+      const lastProductCard = document.querySelector(".product-card:last-of-type");
+  
+      lastProductCard.querySelector("img").src = "./css/img/producto_placeholder.jpg"; // Cambia la imagen de ejemplo por la real
+      lastProductCard.querySelector("h2").textContent = producto.nombre;
+      lastProductCard.querySelector("p:nth-child(3)").textContent = `Precio: $${producto.precio}`;
+      lastProductCard.querySelector("p:nth-child(4)").textContent = producto.descripcion_corta;
+    }
+  });
+  
